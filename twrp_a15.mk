0ac7f2060c29a260c871b3682e410f61d548fc30
BlackNight Shadow <65853201+BNLOS@users.noreply.github.com>
2024-09-24 23:20:54 +0300

    Update twrp_a15.mk

--- twrp_a15.mk
+++ twrp_a15.mk
@@ -1,30 +1,29 @@
-DEVICE_PATH := device/samsung/a15
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-# Release name
-PRODUCT_RELEASE_NAME := a15
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from common AOSP config
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/pb/config/common.mk)
-
-# Inherit some common TWRP stuff.
+# Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit device configuration
-$(call inherit-product, $(DEVICE_PATH)/device.mk)
-
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a10/recovery/root,recovery/root)
+# Inherit from a15 device
+$(call inherit-product, device/samsung/a15/device.mk)
 
 PRODUCT_DEVICE := a15
 PRODUCT_NAME := twrp_a15
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A155F
 PRODUCT_MANUFACTURER := samsung
+
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a15nsxx-user 12 SP1A.210812.016 A155FXXU3BXG4 release-keys"
+
+BUILD_FINGERPRINT := samsung/a15nsxx/a15:12/SP1A.210812.016/A155FXXU3BXG4:user/release-keys

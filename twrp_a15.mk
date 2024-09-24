2503b205081721ffb1c6b360f5858586b0e63f5c
BlackNight Shadow <65853201+BNLOS@users.noreply.github.com>
2024-09-24 22:36:48 +0300

    Update twrp_a15.mk

--- twrp_a15.mk
+++ twrp_a15.mk
@@ -4,8 +4,11 @@ DEVICE_PATH := device/samsung/a15
 PRODUCT_RELEASE_NAME := a15
 
 # Inherit from common AOSP config
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -13,15 +16,15 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit device configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a10/recovery/root,recovery/root)
+
 PRODUCT_DEVICE := a15
 PRODUCT_NAME := twrp_a15
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A155F
 PRODUCT_MANUFACTURER := samsung
-
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a15nsxx-user 12 SP1A.210812.016 A155FXXU3BXG4 release-keys"
-
-BUILD_FINGERPRINT := samsung/a15nsxx/a15:12/SP1A.210812.016/A155FXXU3BXG4:user/release-keys
